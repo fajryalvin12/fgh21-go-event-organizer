1a3b5c2fe6ddc5cd0b55c143d62ad058abe5b5c2
fajryalvin12 <fajryalvin12@gmail.com>
2024-09-16 13:25:08 +0700

    fix: config the db.go and makefile

--- lib/db.go
+++ lib/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func DB() *pgx.Conn {
-	connString := "postgresql://postgres:1@172.17.0.2:5432/event_organizer?sslmode=disable"
+	connString := "postgresql://postgres:1@103.93.58.89:54322/event_organizer?sslmode=disable"
 	conn, _ := pgx.Connect(
 		context.Background(),
 		connString,
